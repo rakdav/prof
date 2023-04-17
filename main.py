0e6a7584964686a8b3a5469ad734db26b673c182
Andrey <rakdav@inbox.ru>
2023-04-17 20:22:36 +0200

    17_04_2023_1

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from PyQt5 import QtWidgets
+
+import autorize
 import choice
 import sys
 import group
+import reg
 import single
 
 
@@ -15,11 +18,11 @@ class win_choice(QtWidgets.QWidget, choice.Ui_Form):
         self.single_win = None
 
     def btn_group_click(self):
-        self.group_win = group_window()
+        self.group_win = log_in_window()
         self.group_win.show()
 
     def btn_single_click(self):
-        self.single_win = group_window()
+        self.single_win = log_in_window()
         self.single_win.show()
 
 
@@ -30,6 +33,18 @@ class group_window(QtWidgets.QWidget, group.Ui_Form):
 
 
 class single_window(QtWidgets.QWidget, single.Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+class log_in_window(QtWidgets.QWidget, autorize.Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+class reg_window(QtWidgets.QWidget, reg.Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
