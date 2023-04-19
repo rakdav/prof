af65356905ec737aa8f61946b38486f47101c599
Andrey <rakdav@inbox.ru>
2023-04-19 08:31:49 +0200

    19_04

---
+++ classUser.py
@@ -0,0 +1,14 @@
+class User:
+    def __int__(self, first_name, second_name, last_name, phone, email, birthday, series_num, login,
+                password, data, id_group):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.last_name = last_name
+        self.phone = phone
+        self.email = email
+        self.birthday = birthday
+        self.series_num = series_num
+        self.login = login
+        self.password = password
+        self.date = data
+        self.id_group = id_group
--- main.py
+++ main.py
@@ -39,32 +39,51 @@ class single_window(QtWidgets.QWidget, single.Ui_Form):
         self.User = User
         self.setupUi(self)
         if self.User is not None:
-            pass
+            self.le_sur.setText(User['first_name'])
 
 
 def connect_to_sql_server():
     return Connect.Connect("HOMEPC\SQLEXPRESS", "main_db", "user1", "user1").get_connect()
 
 
+class QtMessageBox:
+    pass
+
+
 class log_in_window(QtWidgets.QWidget, autorize.Ui_Form):
     def __init__(self):
         super().__init__()
+        self.win = None
         self.setupUi(self)
+        self.le_login.setText("Prohor156")
+        self.le_password.setText("zDdom}SIhWs?")
         self.pb_enter.clicked.connect(self.pb_enter_click)
 
     def pb_enter_click(self):
         db_connect = connect_to_sql_server()
         db_connect.open()
         query = QSqlQuery(db_connect)
-        text_query = f"SELECT first_name FROM Users WHERE login='{self.le_login.text()}' " \
+        text_query = f"SELECT * FROM Users WHERE login='{self.le_login.text()}' " \
                      f"AND password='{self.le_password.text()}'"
-        print(text_query)
         query.prepare(text_query)
         query.exec()
-        if query.record() != 0:
+        if query.numRowsAffected():
             query.first()
-            print(query.value("first_name"))
-
+            user = {
+                "first_name": query.value('first_name'), "second_name": query.value('second_name'),
+                "last_name": query.value('last_name'), "phone": query.value('phone'),
+                "email": query.value("email"), "birthday": query.value("birthday"),
+                "series_num": query.value("series_num"), "login": query.value("login"),
+                "password": query.value("password"), "date": query.value("date"),
+                "id_group": query.value("id_group")
+            }
+            if query.value("id_group") == 0:
+                self.win = single_window(user)
+                self.win.show()
+        else:
+            message = QtWidgets.QMessageBox()
+            message.setText("None")
+            message.show()
         db_connect.close()
 
 
